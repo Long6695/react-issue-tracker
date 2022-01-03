4e0a03a278b0d8c8f5aedef0e27efdd19d8e57db
Long <thaihothanhlong95@gmail.com>
2022-01-03 22:34:39 +0700

    add debounce to search value input

--- src/features/issue/components/SearchIssueTracker.jsx
+++ src/features/issue/components/SearchIssueTracker.jsx
@@ -1,14 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+// library
 import { Form } from 'react-bootstrap'
+// context
 import { useIssueContext } from 'context/issueTrackerContext'
 
 const SearchIssueTracker = () => {
   const { setSearchText } = useIssueContext()
+  const [saveText, setSaveText] = useState('')
 
   const debounceRef = useRef(null)
 
   const handleSearchValue = (e) => {
     const value = e.target.value
+
+    setSaveText(value)
 
     if (debounceRef.current) {
       clearTimeout(debounceRef.current)
@@ -23,9 +28,9 @@ const SearchIssueTracker = () => {
     <div className="d-flex justify-content-between align-items-center">
       <h3 className="w-100">List Todo</h3>
       <Form.Control
-        ref={debounceRef}
         className="w-25"
         type="text"
+        value={saveText}
         placeholder="Search by description..."
         onChange={handleSearchValue}
       />
